848457ac32225d5a5b7eb4094b601be55caa95e8
Vinicius-G-R <125162352+Vinicius-G-R@users.noreply.github.com>
2023-04-28 18:26:34 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿double x, y;
+    Console.Clear();
+    Console.WriteLine("--- Quadrante Cartesiano ---\n");
+    Console.WriteLine("Entre com o ponto desejado.");
+    Console.Write("Coordenada x: ");
+    x = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Coordenada y: ");
+    y = Convert.ToDouble(Console.ReadLine());
+     
+    Console.Write($"\nO ponto ({x}, {y}) fica ");
+
+    if (x > 0 && y > 0) 
+    Console.WriteLine("no quadrante 1");
+    
+    else if (x < 0 && y > 0)
+    Console.WriteLine("no quadrante 2");
+        
+    else if (x < 0 && y < 0)
+    Console.WriteLine("no quadrante 3");
+
+    else if (x > 0 && y < 0)
+    Console.WriteLine("no quadrante 4");
+    
+    else if (x == 0 && y != 0)
+    Console.WriteLine("no eixo das ordenadas.");
+    
+    else if (x != 0 && y == 0)
+    Console.WriteLine("no eixo das abscissas.");
+      
+    else 
+    Console.WriteLine("no ponto de origem");
